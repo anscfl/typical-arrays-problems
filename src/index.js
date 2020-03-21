3dc23488caa850e5c078ef10ed81be8acf21bbbf
anscfl <60544277+anscfl@users.noreply.github.com>
2020-03-21 16:36:13 +0400

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0){
+    return 0;
+  }
+  else {
+    let MIN = 9999999;
+    for(let a of array){
+      if(a < MIN){
+        MIN = a;
+      }
+    }
+    return MIN;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0){
+    return 0;
+  }
+  else{
+    let MAX = -9999999;
+    for(let a of array){
+      if(a > MAX){
+        MAX = a;
+      }
+    }
+    return MAX;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0){
+    return 0;
+  }
+  else{
+    let sum = 0, AVG = 0, i;
+    for(i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    return AVG = sum/i;
+  }
 }
